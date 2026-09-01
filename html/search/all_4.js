84efa5f2cdc4caeac91697ffea5f24bbf19eac6b
Hibikus <hayleyjadenel@gmail.com>
2026-09-01 08:44:51 +0200

    Removed getStartTime() and getFinishTime()

--- html/search/all_4.js
+++ html/search/all_4.js
@@ -1,9 +1,7 @@
 var searchData=
 [
-  ['gate_0',['gate',['../classGate.html',1,'Gate'],['../classGate.html#ad2a1d6cc13043ab8bb2138dbe2eee3c1',1,'Gate::Gate()']]],
+  ['gate_0',['gate',['../classGate.html',1,'Gate'],['../classGate.html#a9abc622ac27a586585085c663932f799',1,'Gate::Gate()']]],
   ['getcapacity_1',['getCapacity',['../classCrowd.html#a68245beefe0bc1ea1a6b20c682e518ac',1,'Crowd']]],
-  ['getfinishtime_2',['getFinishTime',['../classArtist.html#a8b7987b0499f32420bec462e3a76f67a',1,'Artist']]],
-  ['getschedule_3',['getSchedule',['../classMusicZone.html#aab5c738f54afb39cc929c7fd5b54dcdf',1,'MusicZone']]],
-  ['getstagename_4',['getStageName',['../classMusicZone.html#a5d0e429e65e00194660d076531eb6059',1,'MusicZone']]],
-  ['getstarttime_5',['getStartTime',['../classArtist.html#a3feade25fb097346b69e1cae64310cb2',1,'Artist']]]
+  ['getschedule_2',['getSchedule',['../classMusicZone.html#aab5c738f54afb39cc929c7fd5b54dcdf',1,'MusicZone']]],
+  ['getstagename_3',['getStageName',['../classMusicZone.html#a5d0e429e65e00194660d076531eb6059',1,'MusicZone']]]
 ];
